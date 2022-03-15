56e3c1273ad74a9aee1f99b7b4cd1e75038d088f
aamiryaqoob <aamiryaqoob965@gmail.com>
2022-03-16 01:01:18 +0500

    Some changes in ExpenseItem.js

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,7 +1,7 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-// This is date component
+// Date component
   const month = props.date.toLocaleString('en-US' , { month: 'long'});
   const day = props.date.toLocaleString('en-US' , { day: '2-digit'});
   const year = props.date.getFullYear();
