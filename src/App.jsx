8210222f3533d77da63e6ab5bf1fdd33691d5706
aamiryaqoob <aamiryaqoob965@gmail.com>
2022-04-06 01:31:09 +0500

    some changes in newexpense and expenseform

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,14 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  const addExpenseHandler = expense => {
+    console.log('In app.js');
+    console.log(expense);
+  };
 
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
 
       <ExpenseItem
         title={expenses[0].title}
