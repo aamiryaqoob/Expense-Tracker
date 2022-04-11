7f5475cac67161adc3a5b9337222e2d0736479c2
aamiryaqoob <aamiryaqoob965@gmail.com>
2022-04-11 23:57:04 +0500

    Added Expenes.jsx and Expenses.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 import ExpensesFilter from "./components/NewExpense/ExpenseFilter";
 
 import NewExpense from "./components/NewExpense/NewExpense";
@@ -35,26 +35,7 @@ function App() {
     <div>
       <NewExpense onAddExpense={addExpenseHandler}/>
       <ExpensesFilter />
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses items={expenses}/>
     </div>
   );
 };
