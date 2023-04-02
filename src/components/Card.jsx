8cbf42abf4648f31deb417806710c550a42f2cd1
aamiryaqoob <aamiryaqoob965@gmail.com>
2023-04-02 23:25:41 +0500

    card added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Card.css';
 
+//card page
+
 const Card = (props) => {
 
     const classes = 'card ' + props.className;
-    
+
   return (
     
     <div className={classes}>{props.children}</div>
