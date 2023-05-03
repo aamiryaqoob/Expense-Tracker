5504736813847bb42429fbad5cfac4d4037e322e
aamiryaqoob <aamiryaqoob965@gmail.com>
2023-05-04 01:07:54 +0500

    Changes in ExpenseItem.jsx

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,23 +1,18 @@
-import react, { useState } from 'react';
+import react from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
   
-  const clickHandler = (props) => {
-    setTitle('Updated!!!!')
-    
-  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
-      <button onClick={clickHandler}>Change title</button>
+      
     </Card>
     
   );
